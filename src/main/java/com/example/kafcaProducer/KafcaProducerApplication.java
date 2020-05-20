be6f2597d927fe00d3352c87e4589174794cb2fd
Ravindar Thirugnanasambandam <ravindar.t.j2ee@gmail.com>
2020-05-20 14:23:47 -0400

    commit

---
+++ src/main/java/com/example/kafcaProducer/KafcaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafcaProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafcaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafcaProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafcaProducer/KafcaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafcaProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafcaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
